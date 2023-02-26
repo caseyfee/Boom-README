71719f64d782dcf60eaec9eaeb4c712314761f0a
caseyfee <caseyfee3@gmail.com>
2023-02-25 20:04:15 -0800

    added questions

--- index.js
+++ index.js
@@ -2,7 +2,33 @@
 
 // TODO: Create an array of questions for user input
 const questions = [
-    
+    {question: "Please enter your project title?", 
+    answer: "",
+    },
+    {question: "What would you like to include in the README Description?", 
+    answer: "",
+    },
+    {question: "What are the installation instructions?", 
+    answer: "",
+    },
+    {question: "What usage information would you like to include?", 
+    answer: "",
+    },
+    {question: "What are the contribution guidelines?", 
+    answer: "",
+    },
+    {question: "What are the test instrutions?", 
+    answer: "",
+    },
+    {question: "What license would you like to use?", 
+    answer: "",
+    },
+    {question: "What is your gitHub username?", 
+    answer: "",
+    },
+    {question: "What is your email?", 
+    answer: "",
+    },  
 ];
 
 // TODO: Create a function to write README file
