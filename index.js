891d83c58a80e8b923014749aa4a85405b324436
caseyfee <caseyfee3@gmail.com>
2023-02-26 18:24:07 -0800

    testing questions and writetoFile

--- index.js
+++ index.js
@@ -1,41 +1,79 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require("fs");
+
+// nodemon??
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {question: "Please enter your project title?", 
-    answer: "",
+inquirer.prompt([
+    {   type: "input",
+        name: "title",
+        message: "Please enter your project title?", 
     },
-    {question: "What would you like to include in the README Description?", 
-    answer: "",
+    {   type: "input",
+        name: "description",
+        message: "What would you like to include in the README Description?", 
     },
-    {question: "What are the installation instructions?", 
-    answer: "",
-    },
-    {question: "What usage information would you like to include?", 
-    answer: "",
-    },
-    {question: "What are the contribution guidelines?", 
-    answer: "",
-    },
-    {question: "What are the test instrutions?", 
-    answer: "",
-    },
-    {question: "What license would you like to use?", 
-    answer: "",
-    },
-    {question: "What is your gitHub username?", 
-    answer: "",
-    },
-    {question: "What is your email?", 
-    answer: "",
-    },  
-];
+//     {   type: "input",
+//         name: "instructions",
+//         message: "What are the installation instructions?", 
+//    },
+//     {   type: "input",
+//         name: "usage",
+//         message: "What usage information would you like to include?", 
+        
+//     },
+//     {   type: "input",
+//         name: "contribution",
+//         message: "What are the contribution guidelines?", 
+        
+//     },
+//     {   type: "input",
+//         name: "instructions",
+//         message: "What are the test instructions?", 
+//     },
+//     {   type: "list",  
+//         name: "license",
+//         message: "What license would you like to use?", 
+//         choices: ["MIT", "ISC", "None"],
+        
+//     },
+//     {   type: "input",
+//         name: "gitHub",
+//         message: "What is your gitHub username?", 
+//     },
+//     {   type: "input",
+//         name: "email",
+//         message: "What is your email?", 
+//     },  
+])
+.then((data) => {
+    console.log(data);
+
+    
+    const filename = `${data.title.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('filename created!')
+    );
+    writeToFile(data);
+}   
+); 
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+
+    return `
+    # ${data.name}`
+  
+
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
